19836bddbc34a9cc92da6bc6aae89ec51e7c5665
Jack Whitesell <me@jackwhitesell.com>
2020-12-30 16:14:17 -0800

    removing notification works, and added form tests

--- notifications/forms.py
+++ notifications/forms.py
@@ -1,4 +1,8 @@
 from django import forms
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class SearchForm(forms.Form):
-    terms = forms.CharField(label='Search terms', max_length=100)
+    terms = forms.CharField(label='Search terms', max_length=100, min_length=6)
+
+class RemoveForm(forms.Form):
+    pk = forms.IntegerField(validators=[MinValueValidator(0)])
--- notifications/views.py
+++ notifications/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
-from .forms import SearchForm
+from .forms import SearchForm, RemoveForm
 from .models import Search    
 
 def index(request):
     current_user = request.user
-    data = Search.objects.filter(user_id=current_user.id)
-    return render(request, 'notifications/index.html', {'data': data, 'user': current_user})
+    current_objects = Search.objects.filter(user_id=current_user.id)
+    
+    if request.method == 'POST':
+        form = RemoveForm(request.POST)
+        if form.is_valid():
+            s = Search.objects.get(pk=form.cleaned_data['pk'])
+            if current_user.id == s.user_id:
+                s.delete()
+        return HttpResponseRedirect('/notifications/')
+    else:
+        data = []
+        for i in range(len(current_objects)):
+            tup = (current_objects[i], RemoveForm(initial={'pk': current_objects[i].pk}))
+            data.append(tup)
+        return render(request, 'notifications/index.html', {'data': data, 'user': current_user})
 
 def new_search(request):
     if request.method == 'POST':
@@ -15,8 +28,7 @@ def new_search(request):
         if form.is_valid():
             s = Search(terms=form.cleaned_data['terms'], user_id=request.user.id)
             s.save()
-            return HttpResponseRedirect('/notifications/')
+        return HttpResponseRedirect('/notifications/')
     else:
         form = SearchForm()
-    
-    return render(request, 'notifications/new.html', {'form': form})
+        return render(request, 'notifications/new.html', {'form': form})
